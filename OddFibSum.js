7321a6b4421e7e8f19056c993d56aaef5ab7b46f
Andres <floresc.andres@intracitygeeks.org>
2018-10-02 00:30:54 -0400

    Adding odd numbers in Fibonacci sequence

--- OddFibSum.js
+++ OddFibSum.js
@@ -1,50 +1,25 @@
 function sumFibs(num) {
-  let prev;
-  let newNum;
+  let first = 1;
+  let second = 1;
   let oddFibSum;
+  
+  if (num < 3){
+    oddFibSum = num;
+  } else {
+    oddFibSum = first + second;
+    
+    do
+    {
+      second += first;
+      first = second - first;
 
-  if (num > 1) {
-    prev = 1;
-    newNum = 1;
-    oddFibSum = 1;
-
-    for (let i = 0; i < num; i++) {
-      newNum += prev;
-      prev -= newNum;
-
-      if (newNum % 2 != 0 && newNum <= num) {
-        oddFibSum += newNum
+      if (second <= num && second % 2 != 0) {
+        oddFibSum += second;
       }
     }
-  } else {
-    oddFibSum = 1;
+    while (second < num)
   }
-  
   return oddFibSum;
 }
 
-sumFibs(4);
-
-function sumFibs(num) {
-  let first = 2;
-  let hold;
-  let secondHold;
-  let OddFibSum;
-  
-  if (num < 3){
-    OddFibSum = num;
-  } else {
-    OddFibSum = first;
-    for(let i = 2; i <= num; i++){
-      hold += first;
-      first = 1;
-      
-      if (hold == 1 || hold % 2 != 0) {
-        OddFibSum += hold;
-      }
-    }
-  }
-  return OddFibSum;
-}
-
-sumFibs(4);
+sumFibs(5);
