6171d88b682bfc165ab3c4094bd194a7aa67996b
Andres <floresc.andres@intracitygeeks.org>
2018-11-26 22:58:59 -0500

    permutations

---
+++ permutations.js
@@ -0,0 +1,44 @@
+function permAlone(str) {
+  let arrayOfLetters = str.split('');
+  let stringLength = arrayOfLetters.length;
+  let permutations = stringLength;
+  let factorial = stringLength;
+
+  if (stringLength > 1){
+    while (factorial > 1){
+        permutations = permutations * (factorial - 1);
+        factorial--;
+      }
+  }
+  
+  console.log(permutations);
+
+  let repeats = 0;
+
+  for (let i = 0; i < permutations; i++){
+    
+    let floatingArray = [];
+    
+    for (let k = 0; k < stringLength; k++){
+        floatingArray.push(arrayOfLetters[k]);
+    }
+    
+    let letterToCompare = floatingArray.splice(i, 1);
+
+    let floatArrayLength = floatingArray.length;
+
+    for (let j = 0; j < floatArrayLength; j++){
+        if (letterToCompare[0] == floatingArray[j]){
+            repeats++;
+        }
+    }
+  }
+  
+  if(repeats > 0){
+    permutations -= repeats * 2;
+  }
+
+  return permutations;
+}
+
+permAlone('aab');
